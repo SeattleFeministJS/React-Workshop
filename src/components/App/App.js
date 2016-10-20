4f7f7b378f7eb2b1dbc54ecfc2408f35654f519c
simplesthing <i.am.simplesthing@gmail.com>
2016-10-19 21:11:27 -0700

    add emojis :fire: and animated gradients

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import s from './app.css'
+import animatedGradient from '../../lib/animated-gradient'
 
 export default class App extends Component {
+
+  componentDidMount(){
+    animatedGradient('animate')
+  }
+
   render() {
     return (
-      <div className={s.app}>{this.props.children}</div>
+      <div className={s.app} id='animate'>{this.props.children}</div>
      )
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App/App'
 
-const HelloWorld = (<div><h1>Hello CSS</h1></div>)
+const HelloWorld = (<div><h1>Hello CSS <i className="em em-heart_eyes"></i></h1></div>)
 
 ReactDOM.render(<App>{HelloWorld}</App>, document.getElementById('app'))
