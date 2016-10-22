418a631c75ff3fec8ea29ca10f844d379d6e1ec1
Jessica Wicksnin <jwicksnin@gmail.com>
2016-10-21 21:15:12 -0700

    Add static text input and select elements to the Form.

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,6 +8,14 @@ export default class Form extends Component {
   render() {
     return <form>
       <div>Please sign up!</div>
+      <input type="text" placeholder="Your name here"/>
+      <label for="feelings">How are you feeling today?
+	      <select id="feelings">
+	    		<option value="Contemplative">Contemplative</option>
+	    		<option value="Sleepy">Sleepy</option>
+	    		<option value="Excited">Excited</option>
+	  		</select>
+  		</label>
       <label>Check this box if you're learning React.js
         <input 
           type="checkbox" 
